7bee5c8cd23c049d4ba541c9b30f95561e31b6aa
Freddie <pe-nf@hotmail.com>
2026-07-06 10:42:02 -0400

    Temporarily remove host payouts card from overview

--- src/pages/overview/OverviewPage.tsx
+++ src/pages/overview/OverviewPage.tsx
@@ -1,5 +1,5 @@
 
-
+import { useEffect, useState } from "react";
 import { PmsControlCenter } from "../../components/dashboard/PmsControlCenter";
 import { LocksCapacityCard } from "../../components/dashboard/LocksCapacityCard";
 
